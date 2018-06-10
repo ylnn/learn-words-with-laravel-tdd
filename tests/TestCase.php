255efe1c668502007bfaa31ba4bbafb4e7d6f708
yalin <yalincobanoglu@yandex.com>
2018-06-10 12:53:41 +0300

    test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,26 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Faker\Factory as Faker;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations, DatabaseTransactions;
+
+    protected $faker;
+
+/*     public function setUp()
+    {
+        parent::setUp;
+        $this->faker = Faker::create();
+    }
+
+    public function tearDown()
+    {
+        $this->artisan('migrate:reset');   
+        parent::tearDown();
+    } */
+    
 }
