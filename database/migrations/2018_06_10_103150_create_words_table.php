0ca4e69b596a281a01b0c624e61a7a43b5f2468d
yalin <yalincobanoglu@yandex.com>
2018-06-10 14:14:00 +0300

    words migration

---
+++ database/migrations/2018_06_10_103150_create_words_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('word')->index();
+            $table->string('mean');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
