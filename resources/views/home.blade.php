4f5ffefcfa2aec517fa07b2740e2adc936a2f92d
yalin <yalincobanoglu@yandex.com>
2018-06-10 14:37:14 +0300

    home blade show words

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,30 @@
                     <div>
                         <a href="{{route('word.create')}}" class="btn btn-info">Add New</a>
                     </div>
+                    <div class="pt-3">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Word</th>
+                                    <th>Mean</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($words as $word)
+                                    <tr>
+                                        <td>{{$word->word}}</td>
+                                        <td>{{$word->mean}}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="2">
+                                           No words.
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 </div>
             </div>
