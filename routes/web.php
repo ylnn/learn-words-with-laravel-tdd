4709e558c9faaa6b8437b41493fa26566498aeb1
yalin <yalincobanoglu@yandex.com>
2018-06-10 16:30:29 +0300

    delete route

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/word/create', 'Front\WordController@create')->name('word.create');
-
 Route::post('/word/store', 'Front\WordController@store')->name('word.store');
+Route::get('/word/delete/{id}', 'Front\WordController@delete')->name('word.delete');
