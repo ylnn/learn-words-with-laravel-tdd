fb52dceb4802fa9dd0831e6bcbd997ec26f15501
yalin <yalincobanoglu@yandex.com>
2018-06-10 16:30:05 +0300

    auto foucs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,4 +101,12 @@
 
     </div>
 </body>
+
+<script src="{{asset('js/jquery-3.3.1.slim.min.js')}}"></script>
+
+<script>
+    $( document ).ready(function() {
+        $('#word').focus()
+    })
+</script>
 </html>
