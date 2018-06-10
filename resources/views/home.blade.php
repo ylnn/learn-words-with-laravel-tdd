0922f59b6af801a9c7bce11370a6ec082fec4893
yalin <yalincobanoglu@yandex.com>
2018-06-10 16:30:21 +0300

    delete feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,16 @@
             <div class="card">
                 <div class="card-header">All Words</div>
                 <div class="card-body">
-                    <div>
-                        <a href="{{route('word.create')}}" class="btn btn-info">Add New</a>
-                    </div>
+                    
                     <div class="pt-3 pb-3">
                         <div class="row">
                             <div class="col-6">
                                 <form action="{{route('home')}}" method="GET">
                                     <input type="text" class="form-control" name="filter" placeholder="Filter...">
                                 </form>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end">
+                                <a href="{{route('word.create')}}" class="btn btn-info">Add New</a>
                             </div>
                         </div>
                     </div>
@@ -22,6 +23,7 @@
                                 <tr>
                                     <th>Word</th>
                                     <th>Mean</th>
+                                    <th>Act</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -29,6 +31,7 @@
                                     <tr>
                                         <td>{{$word->word}}</td>
                                         <td>{{$word->mean}}</td>
+                                        <td class="d-flex justify-content-end"><a href="{{route('word.delete', ['id' => $word->id])}}" class="btn btn-danger">Delete</a></td>
                                     </tr>
                                 @empty
                                     <tr>
