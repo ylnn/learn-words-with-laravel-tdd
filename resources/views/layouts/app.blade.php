3bc6aac2efb5f8caa83aac7af810bd81c7a63fbd
yalin <yalincobanoglu@yandex.com>
2018-06-11 15:51:41 +0300

    removed jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,7 @@
     </div>
 </body>
 
-<script src="{{asset('js/jquery-3.3.1.slim.min.js')}}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
 
 <script>
     $( document ).ready(function() {
