f0d1c59a1d18a2e259d39f2155239e555d4715a1
yalin <yalincobanoglu@yandex.com>
2018-06-21 13:55:08 +0300

    welcome links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Learnwords.space | Learn the words you find most diffucult to learn</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -17,12 +17,12 @@
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
-                height: 100vh;
+                height: 50vh;
                 margin: 0;
             }
 
             .full-height {
-                height: 100vh;
+                height: 80vh;
             }
 
             .flex-center {
@@ -35,28 +35,39 @@
                 position: relative;
             }
 
-            .top-right {
+            /* .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
-            }
+            } */
 
             .content {
                 text-align: center;
             }
 
             .title {
-                font-size: 84px;
+                font-size: 54px;
+                font-weight: bold;
+            }
+
+            .welcome-desc {
+                font-size: 30px;
+            }
+
+            .links {
+                padding-top: 30px;
             }
 
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
-                font-size: 12px;
+                font-size: 22px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
+                border: 1px solid silver;
+                padding: 20px;
             }
 
             .m-b-md {
@@ -66,23 +77,27 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Learn Word
+                    Learnwords.space
                 </div>
-                <p>Easy way to learn new words.</p>
+                <p class="welcome-desc">Learn the words you find most diffucult to learn</p>
+
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
+                            <a href="{{ route('register') }}">Register</a>
+                        @endauth
+                    </div>
+                @endif
+
             </div>
+
+
         </div>
     </body>
 </html>
